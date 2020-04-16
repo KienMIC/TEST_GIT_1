c081cb0928b4a4f8b439f4fd19bdcd6d2b10328a
Ngô Trung Kiên <kienhp1hp@gmail.com>
2020-04-16 22:55:26 +0700

    TAI FILE LEN REPO TEST GIT

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DETERMINE_CLASS_NETWORK
+{
+    class Program
+    {
+        public static char FindClass(int ip)
+        {
+            if (ip >= 1 && ip <= 126)
+                return 'A';
+
+            else if (ip >= 128 && ip <= 191)
+                return 'B';
+
+            else if (ip >= 192 && ip <= 223)
+                return 'C';
+
+            else if (ip >= 224 && ip <= 239)
+                return 'D';
+
+            else
+                return 'E';
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**DETERMINE CLASS OF IPv4 ADDRESS - CODING BY NGO TRUNG KIEN - 74458**");
+            string ip = "";     
+            Console.WriteLine("Please Enter the IPv4 Address: ");
+            ip = Console.ReadLine();
+            string[] arr = ip.Split('.');
+            int ip1= Convert.ToInt32(arr[0]);
+            Console.WriteLine("IP Address belongs to Class " + FindClass(ip1));
+
+        }
+    }
+}
